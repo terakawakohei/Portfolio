5a54f6db98c052177042137fc4fe5c5bb33fcccc
odyklife <trivover@icloud.com>
2020-03-02 16:58:02 +0900

    first commit

---
+++ src/components/ShowImage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="">
+    <div class="parent">
+      <img
+        class="imgsize"
+        alt="イラストが表示されます"
+        :src="LoadImg(firstIMG)"
+      />
+      <img
+        class="imgsize"
+        alt="イラストが表示されます"
+        :src="LoadImg(secondIMG)"
+      />
+
+      <img
+        class="imgsize"
+        alt="イラストが表示されます"
+        :src="LoadImg(thirdIMG)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    firstIMG: String,
+    secondIMG: String,
+    thirdIMG: String,
+  },
+  methods: {
+    LoadImg(name) {
+      return require(`../assets/` + name);
+    },
+  },
+  data: function() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.imgsize {
+  width: 20%; /* 横幅を割合で指定 */
+  height: 20%;
+}
+.parent {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <img class="imgsize" alt="Vue logo" src="../assets/netu.png" />
+    <HelloWorld msg="Welcome to My Portfolio!!" />
+    <ShowImage
+      firstIMG="mi-ha-.png"
+      secondIMG="riamu.jpg"
+      thirdIMG="free.png"
+    />
+    <ul>
+      <li v-for="user in users" v-bind:key="user.name">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import ShowImage from '@/components/ShowImage.vue';
+
+export default {
+  name: 'home',
+  data: function() {
+    return {
+      users: [],
+    };
+  },
+
+  components: {
+    HelloWorld,
+    ShowImage,
+  },
+  mounted: function() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users', {
+        params: {
+          name: 'Glenna Reichert',
+        },
+      })
+      .then(response => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.headers);
+        console.log(response.statusText);
+        console.log(response.config);
+      })
+      .catch(response => console.log(response));
+  },
+};
+</script>
+<style>
+.imgsize {
+  width: 30%; /* 横幅を割合で指定 */
+  height: auto;
+}
+</style>
